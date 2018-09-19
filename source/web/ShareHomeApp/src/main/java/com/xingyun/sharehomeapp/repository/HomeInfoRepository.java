4b0fb17c4aa36348534a2384259fff98731e0943
hades_fairy <fairy_xingyun@hotmail.com>
2018-09-19 08:16:05 +0800

    removed source

--- source/web/ShareHomeApp/src/main/java/com/xingyun/sharehomeapp/repository/HomeInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xingyun.sharehomeapp.repository;
-
-import com.xingyun.sharehomeapp.model.db.HomeInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HomeInfoRepository extends JpaRepository<HomeInfo,Long>{
-}
